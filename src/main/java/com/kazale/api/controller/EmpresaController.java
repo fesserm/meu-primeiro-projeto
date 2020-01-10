098b5b82ffc99dbbb3452d7c31c33d2b6ed6f603
p_917726 <fellipe.maciel@camara.leg.br>
2020-01-10 14:41:29 -0300

    add validators

--- src/main/java/com/kazale/api/controller/EmpresaController.java
+++ src/main/java/com/kazale/api/controller/EmpresaController.java
@@ -1,9 +1,18 @@
 package com.kazale.api.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.kazale.api.dto.EmpresaDTO;
+import com.kazale.api.response.Response;
 
 @RestController
 @RequestMapping("/api/empresa")
@@ -12,5 +21,19 @@ public class EmpresaController {
 	@GetMapping(value = "/{nome}")
 	public String exemplo(@PathVariable("nome") String nome) {
 		return "Olá : "+nome;
+	}
+	
+	@PostMapping(value = "/cadastrar")
+	public ResponseEntity<Response<EmpresaDTO>> cadastrar(@Valid @RequestBody EmpresaDTO empresaDto, BindingResult result){
+		Response<EmpresaDTO> response = new Response<EmpresaDTO>();
+		
+		if(result.hasErrors()) {
+			result.getAllErrors().forEach(error -> response.getErros().add(error.getDefaultMessage()));
+			return ResponseEntity.badRequest().body(response);
+		}
+		
+		empresaDto.setId(1L);
+		response.setData(empresaDto);
+		return ResponseEntity.ok(response);
 	}
 }
---
+++ src/main/java/com/kazale/api/dto/EmpresaDTO.java
@@ -0,0 +1,40 @@
+package com.kazale.api.dto;
+
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.br.CNPJ;
+
+public class EmpresaDTO {
+
+	
+	private Long id;
+	private String razaoSocial;
+	private String cnpj;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	@NotEmpty(message = "Razão social não pode ser vazia")
+	@Length(min = 5, max = 200, message = "Razão social deve conter entre 5 e 200 caracteres.")
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+	
+	@NotEmpty(message = "CNPJ não pode ser vazia")
+	@CNPJ(message = "CNPJ inválido.")
+	public String getCnpj() {
+		return cnpj;
+	}
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	
+	
+}
